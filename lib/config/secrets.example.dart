9df350f9a1ebeccd9ee022379b92374b81de17a9
crivigo <vilartortas@gmail.com>
2026-05-19 16:56:23 +0200

    cvilgom2: Vemos el mapa y ocultamos el token

---
+++ lib/config/secrets.example.dart
@@ -0,0 +1,2 @@
+//Copia este archivo como secrets.dart y reemplaza el valor de mapboxAccessToken por tu token de Mapbox
+const String mapboxAccessToken = "pk.PON_AQUI_TU_TOKEN";
--- lib/presentation/screens/charta/charta_screen.dart
+++ lib/presentation/screens/charta/charta_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flu_avm/presentation/widgets/widgets.dart';
 import 'package:flutter/material.dart';
+import 'package:mapbox_maps_flutter/mapbox_maps_flutter.dart';
 
 class ChartaScreen extends StatefulWidget {
   const ChartaScreen({super.key});
@@ -9,25 +10,35 @@ class ChartaScreen extends StatefulWidget {
 }
 
 class _ChartaScreenState extends State<ChartaScreen> {
+
+void _initializeCircleAnnotations(MapboxMap mapboxMap) {
+    // Aquí puedes agregar cualquier configuración adicional para el mapa si es necesario
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Mapas'),
       ),
-      body: const Stack(
+      body: Stack(
         fit: StackFit.expand,
         children: [
-          ColoredBox(color:   Colors.blueGrey,
-          child: Center(
-            child: Text(
-              'Mapa a pantalla completa',
-              style: TextStyle(
-                color: Colors.white,
+            MapWidget(
+              key: const ValueKey('main_map'),
+              cameraOptions:  CameraOptions(
+                center: Point(
+                  coordinates: Position(
+                    -122.467895, // Longitud
+                    37.7749,    // Latitud
+                  ),
+                ),
+                zoom: 12.0,
               ),
+              styleUri: MapboxStyles.MAPBOX_STREETS,
+              onMapCreated: _initializeCircleAnnotations
             ),
-          ),
-        ),
         const Align(
           alignment: Alignment.topRight,
           child: Padding(
